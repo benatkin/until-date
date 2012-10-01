5fab54a2c2155f92a2a4dc5567142a2ff32c4671
Ben Atkin <ben@benatkin.com>
2012-10-01 14:58:04 -0600

    export app and move server into server.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
-  , http = require('http')
   , path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -30,6 +23,4 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+var app = require('./')
+  , http = require('http');
+
+app.set('port', process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
